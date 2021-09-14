08252e138c2385cd080bfd8b4883546d05c471a0
marvin <marvin@thismarvin.com>
2021-09-13 23:08:27 -0500

    feature: created a contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import RepeatRepeatRepeat from "$lib/RepeatRepeatRepeat.svelte";
+</script>
+
+<svelte:head>
+	<title>Contact | thismarvin</title>
+</svelte:head>
+
+<main>
+	<section>
+		<RepeatRepeatRepeat phrase={"contact."} />
+		<h4>Let's get in touch</h4>
+		<ul>
+			<li>
+				<a href="mailto:contact@thismarvin.com">contact@thismarvin.com</a>
+			</li>
+			<li>
+				<a href="https://github.com/thismarvin">github.com/thismarvin</a>
+			</li>
+		</ul>
+	</section>
+</main>
+
+<style lang="scss">
+	section {
+		padding: 2rem 2rem calc(2rem + 60px) 2rem;
+		width: calc(100% - 4rem);
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+
+	li {
+		margin-bottom: 0.5rem;
+	}
+
+	ul li:last-child {
+		margin-bottom: 0;
+	}
+
+	a {
+		display: flex;
+		justify-content: center;
+		font-weight: 600;
+		text-decoration: none;
+		padding: 1rem;
+		width: calc(100% - 2rem);
+		box-shadow: 0.5rem 0.5rem 1rem rgba(#000000, 0.2);
+		color: var(--palette-white);
+		background-color: var(--palette-black);
+	}
+</style>
