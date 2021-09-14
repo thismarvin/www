a1a22fe949d830fb0499f7a5ac2b0fe9e7725b04
marvin <marvin@thismarvin.com>
2021-09-13 23:07:48 -0500

    feature: created a new decorative title component
    Repeat.Repeat.Repeat.
           Repeat.Repeat.Repeat.
                  Repeat.Repeat.Repeat.

---
+++ src/lib/RepeatRepeatRepeat.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let phrase = "repeat";
+
+	const repeated = new Array(3).fill(phrase).join("");
+</script>
+
+<div id="wrapper">
+	<div id="container" class="centered">
+		<ul>
+			<li>{repeated}</li>
+			<li>{repeated}</li>
+			<li>{repeated}</li>
+		</ul>
+	</div>
+</div>
+
+<style lang="scss">
+	ul {
+		margin: 0;
+		padding: 2rem;
+		width: 100%;
+		list-style: none;
+	}
+
+	li {
+		font-weight: 600;
+		font-style: italic;
+	}
+
+	ul li:nth-child(1) {
+		text-align: left;
+	}
+	ul li:nth-child(2) {
+		text-align: center;
+	}
+	ul li:nth-child(3) {
+		text-align: right;
+	}
+
+	#wrapper {
+		position: relative;
+		padding-top: calc(1 / (1 / 1) * 100%);
+	}
+
+	#container {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		box-shadow: 0 0.5rem 1rem rgba(#000000, 0.1);
+	}
+</style>
