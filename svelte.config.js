45e60ea493cb78a67b73d572ecfe0ec890eb5bd7
marvin <marvin@thismarvin.com>
2021-10-29 12:18:06 -0500

    chore: prepared site for netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
+import adapter from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	adapter: adapter(),
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
