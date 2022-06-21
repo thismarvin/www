fd6b53f90b585f35a8fc96e3eef5e33cf15cb4a5
marvin <me@thismarvin.com>
2022-06-20 19:20:20 -0500

    refactor(lib): simplify/improve texture workflow
    The main changes are as follows:
    
    - use `string` instead of `URL` when loading an image
    - always generate mipmaps when creating `WebGLTexture`s
    - `Texture2D`'s constructor no longer requires an `HTMLImageElement`

--- src/lib/texture.ts
+++ src/lib/texture.ts
@@ -1,6 +1,4 @@
-import * as WebGL from "./webgl";
-
-export function createImageFromURL(url: URL): Promise<HTMLImageElement> {
+export function createImageFromURL(url: string): Promise<HTMLImageElement> {
 	return new Promise((resolve, reject) => {
 		try {
 			const image = new Image();
@@ -9,40 +7,25 @@ export function createImageFromURL(url: URL): Promise<HTMLImageElement> {
 				resolve(image);
 			};
 
-			image.src = url.toString();
+			image.src = url;
 		} catch (error) {
 			reject(error);
 		}
 	});
 }
 
-export async function createTextureFromURL(
-	url: URL,
-	gl: WebGL.RenderingContext
-): Promise<Texture2D> {
-	const image = await createImageFromURL(url);
-	const texture = WebGL.createTextureFromImage(gl, image);
-
-	return new Texture2D(image, texture);
-}
-
 export default class Texture2D {
-	public get width(): number {
-		return this.image.width;
-	}
-	public get height(): number {
-		return this.image.height;
-	}
-
-	public readonly image: HTMLImageElement;
+	public readonly width: number;
+	public readonly height: number;
 	public readonly texelWidth: number;
 	public readonly texelHeight: number;
 	public readonly texture: WebGLTexture;
 
-	constructor(image: HTMLImageElement, texture: WebGLTexture) {
-		this.image = image;
-		this.texelWidth = 1 / this.image.width;
-		this.texelHeight = 1 / this.image.height;
+	constructor(width: number, height: number, texture: WebGLTexture) {
+		this.width = width;
+		this.height = height;
+		this.texelWidth = 1 / this.width;
+		this.texelHeight = 1 / this.height;
 		this.texture = texture;
 	}
 }
